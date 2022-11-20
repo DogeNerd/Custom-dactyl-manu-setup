dc91335f3cabd62fb109f1c8a5f6eccd522ae2cd
Doge <PowerFox12.icedragon1113@gmail.com>
2022-11-20 09:45:47 -0500

    Compacted

--- keyboards/pico/handwired/dactyl_manuform/5x6/keymaps/doge/keymap.c
+++ keyboards/pico/handwired/dactyl_manuform/5x6/keymaps/doge/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           KC_LALT, KC_LGUI,     KC_RGUI, XXXXXXX),
 	 [_FURO]    = LAYOUT_5x6(
    KC_GRV,      KC_F1  , KC_F2  , KC_F3  ,KC_F4  , KC_F5  ,     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-   KC_F12,      _______, _______, _______,_______, _______,     _______, KC_PGUP, KC_UP  , KC_PGDN, _______, KC_DEL,
+   KC_F12,      _______, _______, _______,_______, _______,     _______, KC_PGUP, KC_UP  , KC_PGDN, _______, KC_DEL ,
    KC_LSFT,     _______, _______, _______,_______, _______,     _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, KC_CAPS,
    TD(ALT_OSL3),_______, _______, _______,_______, _______,     _______, _______, _______, _______, _______, _______,
                          _______, _______,                                        _______, _______,	
