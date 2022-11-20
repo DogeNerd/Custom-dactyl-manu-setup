8549914471c008c102d9989c1dddca3a3bd14bff
Doge <PowerFox12.icedragon1113@gmail.com>
2022-11-20 09:53:23 -0500

    Getting rid of useless comments

--- keyboards/pico/handwired/dactyl_manuform/5x6/keymaps/doge/rules.mk
+++ keyboards/pico/handwired/dactyl_manuform/5x6/keymaps/doge/rules.mk
@@ -1,18 +1,18 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 
-#Build Options. change yes to no to disable
-VIA_ENABLE = yes 										# Enable VIA support
-# Change Bootmagic from no to lite if you wanna use bootmagic
-BOOTMAGIC_ENABLE = no 						  # Enable Bootmagic Lite
-LTO_ENABLE = yes										# Enables Link Time Optimization
+#Build Options, change yes to no to disable
+VIA_ENABLE = yes
+BOOTMAGIC_ENABLE = no
+LTO_ENABLE = yes
 
-COMBO_ENABLE = yes									# Enables combos
-LEADER_ENABLE = yes									# Enable Leader
-MOUSEKEY_ENABLE = yes								# Mouse keys
-EXTRAKEY_ENABLE = no         				# Audio control and System control
-NKRO_ENABLE = yes										# Enable N-Key Rollover
-TAP_DANCE_ENABLE = yes 							# Enable tap-dance
+# Other Features
+COMBO_ENABLE = yes Enables combos
+LEADER_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = no
+NKRO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
 
 # Sourcing needed base files
 #SRC+=
